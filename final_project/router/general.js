1473dc4722d1c76821096081b2a8190ba29de896
paleta-payaso <estozoide@gmail.com>
2025-03-10 19:17:45 -0400

    add tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,42 +26,72 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/', function (req, res) {
-    res.send(JSON.stringify(books, null, 4));
+public_users.get('/', async (req, res) => {
+    try {
+        const getBooks = async () => books; // Simulating an async function
+        const bookList = await getBooks();
+        res.send(JSON.stringify(bookList, null, 4));
+    } catch (error) {
+        res.status(500).send("Error retrieving books");
+    }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-    const isbn = req.params.isbn;
-    let book = books[isbn];
-    if (book) {
-        res.send(JSON.stringify(book, null, 4));
-    } else {
-        res.send(JSON.stringify({"message": "no match found", "isbn": isbn}, null, 4));
+public_users.get('/isbn/:isbn', async (req, res) => {
+    try {
+        const getBookByISBN = async (isbn) => books[isbn]; // Simulating an async function
+        const isbn = req.params.isbn;
+        const book = await getBookByISBN(isbn);
+
+        if (book) {
+            res.send(JSON.stringify(book, null, 4));
+        } else {
+            res.send(JSON.stringify({ "message": "no match found", "isbn": isbn }, null, 4));
+        }
+    } catch (error) {
+        res.status(500).send("Error retrieving book details");
     }
 });
 
 // Get book details based on author
-public_users.get('/author/:author', function (req, res) {
-    const author = req.params.author;
-    let bookDetails = Object.values(books);
-    bookDetails = bookDetails.filter((book) => book.author === author);
-    if (bookDetails.length > 0) {
-        res.send(JSON.stringify(bookDetails, null, 4));
-    } else {
-        res.send(JSON.stringify({"message": "no match found", "author": author}, null, 4));
+public_users.get('/author/:author', async (req, res) => {
+    try {
+        const getBooksByAuthor = async (author) => {
+            let bookDetails = Object.values(books);
+            return bookDetails.filter((book) => book.author === author);
+        };
+
+        const author = req.params.author;
+        const bookDetails = await getBooksByAuthor(author);
+
+        if (bookDetails.length > 0) {
+            res.send(JSON.stringify(bookDetails, null, 4));
+        } else {
+            res.send(JSON.stringify({ "message": "no match found", "author": author }, null, 4));
+        }
+    } catch (error) {
+        res.status(500).send("Error retrieving books by author");
     }
 });
 
 // Get all books based on title
-public_users.get('/title/:title', function (req, res) {
-    const title = req.params.title;
-    let bookDetails = Object.values(books);
-    bookDetails = bookDetails.filter((book) => book.title === title);
-    if (bookDetails.length > 0) {
-        res.send(JSON.stringify(bookDetails, null, 4));
-    } else {
-        res.send(JSON.stringify({"message": "no match found", "title": title}, null, 4));
+public_users.get('/title/:title', async (req, res) => {
+    try {
+        const getBooksByTitle = async (title) => {
+            let bookDetails = Object.values(books);
+            return bookDetails.filter((book) => book.title === title);
+        };
+
+        const title = req.params.title;
+        const bookDetails = await getBooksByTitle(title);
+
+        if (bookDetails.length > 0) {
+            res.send(JSON.stringify(bookDetails, null, 4));
+        } else {
+            res.send(JSON.stringify({ "message": "no match found", "title": title }, null, 4));
+        }
+    } catch (error) {
+        res.status(500).send("Error retrieving books by title");
     }
 });
 
